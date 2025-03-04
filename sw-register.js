b5eff0b8dadcf490668dd0e67b359d415de241f6
Evan Wallis <evanwallis123@gmail.com>
2025-03-04 15:39:46 -0800

    make the webapp more webappy

---
+++ sw-register.js
@@ -0,0 +1,6 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch(err => console.error("Service Worker registration failed", err));
+  }
+  
